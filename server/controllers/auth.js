549727540cc070f83df1c04b9d1c35da7d549857
antonythomas123 <antonythomas993@gmail.com>
2023-01-22 11:16:48 +0530

    Authorization added and authRoutes set

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 // REGISTER USER
@@ -36,5 +36,23 @@ export const register = async (req, res) => {
         res.status(201).json(savedUser);
     } catch (err) {
         res.status(500).json({ error: err.message })
+    }
+}
+
+/* LOGGIN IN */
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "User does not exist!" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials!" });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 }
