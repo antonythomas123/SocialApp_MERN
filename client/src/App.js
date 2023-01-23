1ba6f05a1573e7e9e612f7faddf3d779d127bde9
antonythomas123 <antonythomas993@gmail.com>
2023-01-23 15:41:27 +0530

    Login Page and Register Page completed

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,21 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from "./theme";
 import HomePage from "./pages/homePage/HomePage";
+import LoginPage from "./pages/loginPage/LoginPage";
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token))
+
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/" element={<HomePage/>}/>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
