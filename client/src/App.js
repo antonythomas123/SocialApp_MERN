4f4960905f7d44082e07399881a36e41d1d6cac2
antonythomas123 <antonythomas993@gmail.com>
2023-01-23 10:58:13 +0530

    Navbar Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import createTheme from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 import { themeSettings } from "./theme";
+import HomePage from "./pages/homePage/HomePage";
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
