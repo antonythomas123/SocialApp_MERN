58fc230206e490d1be301c52a50668b5b3405090
antonythomas123 <antonythomas993@gmail.com>
2023-01-23 09:28:24 +0530

    Dark mode or Light mode theme added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import createTheme from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-  
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
